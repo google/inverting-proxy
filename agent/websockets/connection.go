bfe68afbf4ec6ea650ce732e2a59a1edbf365ad7
Omar Jarjur <ojarjur@google.com>
2018-11-06 18:40:42 -0800

    Document the websockets.Connection API

--- agent/websockets/connection.go
+++ agent/websockets/connection.go
@@ -27,6 +27,11 @@ import (
 	"golang.org/x/net/context"
 )
 
+// Connection implements a websocket client connection.
+//
+// This wraps a websocket.Conn connection as defined by the gorilla/websocket library,
+// and encapsulates it in an API that is a little more amenable to how the server side
+// of our websocket shim is implemented.
 type Connection struct {
 	ctx            context.Context
 	cancel         context.CancelFunc
@@ -34,6 +39,7 @@ type Connection struct {
 	serverMessages chan string
 }
 
+// NewConnection creates and returns a new Connection.
 func NewConnection(ctx context.Context, targetURL string, errCallback func(err error)) (*Connection, error) {
 	ctx, cancel := context.WithCancel(ctx)
 	serverConn, _, err := websocket.DefaultDialer.Dial(targetURL, nil)
@@ -41,8 +47,17 @@ func NewConnection(ctx context.Context, targetURL string, errCallback func(err e
 		log.Printf("Failed to dial the websocket server %q: %v\n", targetURL, err)
 		return nil, err
 	}
-	clientMessages := make(chan string, 10)
+
+	// The underlying websocket library assumes that clients are written to read from the connection
+	// in a tight loop. We prefer to instead have the ability to poll for the next message with a timeout.
+	//
+	// The most direct way to bridge those two approaches is to spawn a goroutine that reads in a
+	// tight loop, and then sends the read messages in to an open channel.
 	serverMessages := make(chan string, 10)
+
+	// Since we are using a channel to pull messages from the connection, we will also use one to
+	// push messages. That way our handling of reads and writes are consistent.
+	clientMessages := make(chan string, 10)
 	var wg sync.WaitGroup
 	wg.Add(2)
 	go func() {
@@ -75,6 +90,8 @@ func NewConnection(ctx context.Context, targetURL string, errCallback func(err e
 			case clientMsg := <-clientMessages:
 				if err := serverConn.WriteMessage(websocket.TextMessage, []byte(clientMsg)); err != nil {
 					errCallback(fmt.Errorf("failed to forward websocket data to the server: %v", err))
+					// Errors writing to the server connection are terminal; once an error is returned
+					// no subsequent calls will succeed.
 					return
 				}
 			}
@@ -99,11 +116,15 @@ func NewConnection(ctx context.Context, targetURL string, errCallback func(err e
 	}, nil
 }
 
+// Close closes the websocket client connection.
 func (conn *Connection) Close() {
 	conn.cancel()
 	close(conn.clientMessages)
 }
 
+// SendClientMessage sends the given message to the websocket server.
+//
+// The returned error value is non-nill if the connection has been closed.
 func (conn *Connection) SendClientMessage(msg string) error {
 	select {
 	case <-conn.ctx.Done():
@@ -114,6 +135,12 @@ func (conn *Connection) SendClientMessage(msg string) error {
 	return nil
 }
 
+// ReadServerMessage reads the next message from the websocket server.
+//
+// The returned error value is non-nill if the connection has been closed.
+//
+// The returned *string value is nil if the error is non-nil, or if the method
+// times out while waiting for a server message.
 func (conn *Connection) ReadServerMessage() (*string, error) {
 	select {
 	case serverMsg, ok := <-conn.serverMessages:
